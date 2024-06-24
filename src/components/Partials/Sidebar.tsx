a158174e396e96ae364cc8d97417dced520d5cf8
fabianazh <fabianazhar216@gmail.com>
2024-06-24 19:51:48 +0700

    Fixing minor bug

--- src/components/Partials/Sidebar.tsx
+++ src/components/Partials/Sidebar.tsx
@@ -36,7 +36,7 @@ export default function Sidebar() {
     return (
         <>
             <aside
-                className={`w-auto fixed h-screen overflow-hidden z-0 bg-white left-0 top-0 p-16 pr-12 hidden lg:flex flex-col gap-8`}
+                className={`w-auto fixed h-screen overflow-hidden z-50 bg-white left-0 top-0 p-16 pr-12 hidden lg:flex flex-col gap-8`}
             >
                 <h1 className="font-black text-6xl">
                     Zufar <span className="block"></span> Syabana
