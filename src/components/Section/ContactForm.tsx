166e283cc1c517b256a0a4cf87a722164f9cccf2
fabianazh <fabianazhar216@gmail.com>
2024-06-25 16:58:34 +0700

    Updating contact page

--- src/components/Section/ContactForm.tsx
+++ src/components/Section/ContactForm.tsx
@@ -24,5 +24,53 @@ export default function ContactForm() {
     function onSubmit(data: FormData) {
         sendEmail(data)
     }
-    return <></>
+    return (
+        <>
+            <form
+                onSubmit={handleSubmit(onSubmit)}
+                className="w-full h-fit flex flex-col gap-4 z-0"
+            >
+                <div className="w-full h-fit flex flex-col">
+                    <div className="w-full grid grid-flow-row lg:grid-flow-col h-fit">
+                        <div className="w-full border border-b-0 lg:border-b lg:border-r-0 border-black h-fit flex-grow">
+                            <input
+                                type="text"
+                                name="name"
+                                id="name"
+                                className="w-full py-3 px-4 focus:border-0 focus:outline-0 focus:ring-0 placeholder:font-normal placeholder:text-stone-500 text-sm lg:text-base"
+                                {...register("name", { required: true })}
+                                autoComplete="false"
+                                placeholder="Nama"
+                            />
+                        </div>
+                        <div className="w-full border border-black h-fit flex-grow">
+                            <input
+                                type="text"
+                                name="email"
+                                id="email"
+                                className="w-full py-3 px-4 focus:border-0 focus:outline-0 focus:ring-0 placeholder:font-normal placeholder:text-stone-500 text-sm lg:text-base"
+                                {...register("email", { required: true })}
+                                autoComplete="false"
+                                placeholder="Email"
+                            />
+                        </div>
+                    </div>
+                    <div className="w-full h-fit border border-t-0 border-black">
+                        <textarea
+                            name="message"
+                            id="message"
+                            rows={6}
+                            className="w-full h-fit resize-none py-3 px-4 focus:border-0 focus:outline-0 focus:ring-0 placeholder:font-normal placeholder:text-stone-500 text-sm lg:text-base"
+                            {...register("message", { required: true })}
+                            placeholder="Pesan"
+                            autoComplete="false"
+                        ></textarea>
+                    </div>
+                </div>
+                <div className="border border-black w-fit py-2 px-8">
+                    <button type="submit">Kirim</button>
+                </div>
+            </form>
+        </>
+    )
 }
