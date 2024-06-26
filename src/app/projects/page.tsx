64fef5a07788c9b15ff51a3481412ae49d989408
fabianazh <fabianazhar216@gmail.com>
2024-06-26 20:21:48 +0700

    Adding data folder

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,43 +1,5 @@
 import PhotoGrid from "@/components/Other/PhotoGrid"
-
-const projects = [
-    {
-        id: 400,
-        thumbnail: "https://via.placeholder.com/300x400",
-        year: "2024",
-        name: "RS. Hermina",
-    },
-    {
-        id: 500,
-        thumbnail: "https://via.placeholder.com/300x500",
-        year: "2024",
-        name: "RS. Hermina",
-    },
-    {
-        id: 600,
-        thumbnail: "https://via.placeholder.com/300x600",
-        year: "2024",
-        name: "RS. Hermina",
-    },
-    {
-        id: 300,
-        thumbnail: "https://via.placeholder.com/400x300",
-        year: "2024",
-        name: "RS. Hermina",
-    },
-    {
-        id: 300,
-        thumbnail: "https://via.placeholder.com/500x300",
-        year: "2024",
-        name: "RS. Hermina",
-    },
-    {
-        id: 300,
-        thumbnail: "https://via.placeholder.com/600x300",
-        year: "2024",
-        name: "RS. Hermina",
-    },
-]
+import { projects } from "@/data/projects"
 
 export default function ProjectsPage() {
     return (
